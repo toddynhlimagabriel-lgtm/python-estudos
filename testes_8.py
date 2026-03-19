b20650107dfa3665109321084a35dc2def51fc44
Yuri Gabriel de Lima <toddynhlimagabriel@gmail.com>
2026-03-18 23:49:33 -0300

    Add files via upload

---
+++ testes_8.py
@@ -0,0 +1,83 @@
+# ========DESAFIO 16========
+# catetoA = float(input('Digite o cateto oposto : '))
+# catetoB = float(input('Digite o cateto adjacente : '))
+# hipotenusa = (catetoA**2 + catetoB**2) **(1/2)
+# print(f'O comprimento da hipotenusa é {hipotenusa:.2f}')
+
+# ========DESAFIO 17========
+# from math import hypot 
+# catetoA = float(input('Digite o cateto oposto: '))
+# catetoB = float(input('Digite o cateto adjacente: '))
+# print(f'A hipotenusa é {hypot(catetoA, catetoB):.2f}')
+
+# ========DESAFIO 18========
+# import math 
+# angulo=float(input('Digite o valor do ângulo: '))
+# rad= math.radians(angulo)
+# seno = math.sin(rad)
+# cosseno = math.cos(rad)
+# tangente = math.tan(rad)
+# print(f'Seno = {seno}')
+# print(f'Cosseno = {cosseno}')
+# print(f'Tangente = {tangente}')
+
+# ========DESAFIO 19========
+# import random 
+# A1 = input('Digite o nome do primeiro aluno : ')
+# A2 = input('Digite o nome do segundo aluno : ')
+# A3 = input('Digite o nome do terceiro aluno : ')
+# A4 = input('Digite o nome do quarto aluno: ')
+# lista= [A1, A2, A3, A4]
+# print(f'O aluno sorteado foi { random.choice (lista) }')
+
+# ========DESAFIO 20========
+# import random 
+# A1 = input('Primeiro aluno: ')
+# A2 = input('Segundo aluno: ')
+# A3 = input('Terceiro aluno: ')
+# A4 = input('Quarto aluno: ')
+# lista= [A1, A2, A3, A4]
+#   # ========DESAFIO 16========
+# catetoA = float(input('Digite o cateto oposto : '))
+# catetoB = float(input('Digite o cateto adjacente : '))
+# hipotenusa = (catetoA**2 + catetoB**2) **(1/2)
+# print(f'O comprimento da hipotenusa é {hipotenusa:.2f}')
+
+# ========DESAFIO 17========
+# from math import hypot (DESAFIO 17)
+# catetoA = float(input('Digite o cateto oposto: '))
+# catetoB = float(input('Digite o cateto adjacente: '))
+# print(f'A hipotenusa é {hypot(catetoA, catetoB):.2f}')
+
+# ========DESAFIO 18========
+# import math (
+# angulo=float(input('Digite o valor do ângulo: '))
+# rad= math.radians(angulo)
+# seno = math.sin(rad)
+# cosseno = math.cos(rad)
+# tangente = math.tan(rad)
+# print(f'Seno = {seno}')
+# print(f'Cosseno = {cosseno}')
+# print(f'Tangente = {tangente}')
+
+# ========DESAFIO 19========
+# import random 
+# A1 = input('Digite o nome do primeiro aluno : ')
+# A2 = input('Digite o nome do segundo aluno : ')
+# A3 = input('Digite o nome do terceiro aluno : ')
+# A4 = input('Digite o nome do quarto aluno: ')
+# lista= [A1, A2, A3, A4]
+# print(f'O aluno sorteado foi { random.choice (lista) }')
+
+# ========DESAFIO 20========
+# import random 
+# A1 = input('Primeiro aluno: ')
+# A2 = input('Segundo aluno: ')
+# A3 = input('Terceiro aluno: ')
+# A4 = input('Quarto aluno: ')
+# lista = [A1, A2, A3, A4]
+# random.shuffle(lista)
+# print(f'A ordem de apresentação será {lista}}')
+
+
+
